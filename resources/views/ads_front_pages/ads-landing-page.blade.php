5639da6bab199988aa222509d117ef7b1dde0d0c
Balwinder Singh <balwinder.singh829@gmail.com>
2026-06-26 13:15:41 +0530

    removed commented code

--- resources/views/ads_front_pages/ads-landing-page.blade.php
+++ resources/views/ads_front_pages/ads-landing-page.blade.php
@@ -1311,27 +1311,6 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
     </div>
 
     @include('ads_front_pages.footer_address')
-
-      <!-- RIGHT SIDE (Existing Content) -->
-      <!-- <div class="col-md-6 text-center text-md-end">
-        <p class="mb-1 fs-5 fw-semibold">
-          E-51, Phase 8, Industrial Area, Mohali, Punjab - 160072
-        </p>
-
-        <div class="d-flex justify-content-md-end justify-content-center gap-3 flex-wrap">
-          <span>
-            <a href="tel:9646522110" class="text-white text-decoration-none">
-              +91 9646522110 , +91 9501381389
-            </a>
-          </span>
-          <span>
-            <a href="mailto:sortiqsolutions@gmail.com" class="text-white text-decoration-none">
-              sortiqsolutions@gmail.com
-            </a>
-          </span>
-        </div>
-      </div> -->
-
     </div>
   </div>
 </section>
